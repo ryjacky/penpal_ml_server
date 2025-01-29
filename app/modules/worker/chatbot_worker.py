804e6c1823ec511369ae504fe6ace4bce43fb441
Jacky Ye <se.jackye@gmail.com>
2025-01-29 22:18:36 +0800

    added chatbot worker files

---
+++ app/modules/worker/chatbot_worker.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+import ssl
+
+from realtime import AsyncRealtimeChannel, AsyncRealtimeClient
+
+from utils import is_bot, insert_message
+
+
+def postgres_changes_callback(payload, *args):
+    print("*: ", payload)
+
+
+def postgres_changes_insert_callback(payload, *args):
+    print("INSERT: ", payload)
+
+
+def postgres_changes_delete_callback(payload, *args):
+    print("DELETE: ", payload)
+
+
+def postgres_changes_update_callback(payload, *args):
+    inbox_id = payload["data"]["record"]["id"]
+    user_id = is_bot(inbox_id)
+    if user_id:
+        insert_message(inbox_id, user_id)
+    print("UPDATE: ", payload)
+
+
+async def test_postgres_changes(socket: AsyncRealtimeClient):
+    await socket.connect()
+
+    # Add your access token here
+    # await socket.set_auth("ACCESS_TOKEN")
+
+    channel = socket.channel("test-postgres-changes")
+
+    await channel.on_postgres_changes(
+
+        "UPDATE", table="inbox", callback=postgres_changes_update_callback
+    ).subscribe()
+
+    await socket.listen()
+
+
+async def main():
+    URL = os.getenv("SUPABASE_URL")
+    JWT = os.getenv("SUPABASE_KEY")
+
+    # Setup the broadcast socket and channel
+    socket = AsyncRealtimeClient(f"{URL}/realtime/v1", JWT, auto_reconnect=True)
+    ssl._create_default_https_context = ssl._create_unverified_context()
+    await socket.connect()
+
+    await test_postgres_changes(socket)
+
+asyncio.run(main())
---
+++ app/modules/worker/utils.py
@@ -0,0 +1,23 @@
+import os
+
+from supabase import create_client, Client
+
+URL = os.getenv("SUPABASE_URL")
+JWT = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(URL, JWT)
+
+def is_bot(inbox_id):
+    user_ids = supabase.table("inbox_participants").select("user_id").eq("inbox_id", inbox_id).execute().data
+    user_id_list = [user_id["user_id"] for user_id in user_ids]
+    for user_id in user_id_list:
+        bot_user = supabase.table("bot_users").select("*").eq("user_id", user_id).execute().data
+        if bot_user:
+            print(f"The updated inbox is an inbox with a bot. {user_id} is the bot user.")
+            return user_id
+    print("The updated inbox is not an inbox with a bot.")
+    return None
+
+def insert_message(inbox_id, user_id):
+    message = {"inbox_id": inbox_id, "content": "I am a bot.", "from_user_id": user_id}
+    supabase.table("messages").insert(message).execute()
