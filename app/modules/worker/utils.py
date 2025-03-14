ad72674c6dfc9dad7bf5fb226374de4cc95e4955
yhecb <148933237+yhecb@users.noreply.github.com>
2025-03-14 15:13:11 +0800

    Update utils.py

--- app/modules/worker/utils.py
+++ app/modules/worker/utils.py
@@ -1,23 +1,39 @@
-import os
-
 from supabase import create_client, Client
+
+from modules.llm.types import Chat
+from modules.llm.llm_client import LLMClient
 
 URL = os.getenv("SUPABASE_URL")
 JWT = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(URL, JWT)
 
-def is_bot(inbox_id):
-    user_id1 = supabase.table("inbox_participants").select("user_id_1").eq("inbox_id", inbox_id).execute().data[0]["user_id_1"]
-    user_id2 = supabase.table("inbox_participants").select("user_id_2").eq("inbox_id", inbox_id).execute().data[0]["user_id_2"]
-    for user_id in [user_id1, user_id2]:
-        bot_user = supabase.table("bot_users").select("*").eq("user_id", user_id).execute().data
-        if bot_user:
-            print(f"The updated inbox is an inbox with a bot. {user_id} is the bot user.")
-            return user_id
-    print("The updated inbox is not an inbox with a bot.")
-    return None
 
-def insert_message(inbox_id, user_id):
-    message = {"inbox_id": inbox_id, "content": "I am a bot.", "from_user_id": user_id}
-    supabase.table("messages").insert(message).execute()
+def get_message(journey_messages_id):
+    message = supabase.table("journey_messages").select("*").eq("id", journey_messages_id).execute().data[0]
+    journey_id = message["journey_id"]
+    content = message["content"]
+    is_from_user = message["is_from_user"]
+    user_id = message["user_id"]
+    return journey_id, content, is_from_user, user_id
+
+
+def get_chats(journey_id):
+    journey_messages = supabase.table("journey_messages").select("*").eq("journey_id", journey_id).execute().data
+    sorted_journey_messages = sorted(journey_messages, key=lambda x: x["id"])
+    chats = []
+    for message in sorted_journey_messages:
+        if message['is_from_user']:
+            chat = Chat(role='user', content=message['content'])
+            chats.append(chat)
+        else:
+            chat = Chat(role='assistant', content=message['content'])
+            chats.append(chat)
+    return chats
+
+
+def insert_message(journey_id, user_id, client: LLMClient):
+    journey_chats = get_chats(journey_id)
+    response = client.get_chat_response(journey_chats)
+    new_message = {"journey_id": journey_id, "content": response, "is_from_user": False, "user_id": user_id}
+    supabase.table("journey_messages").insert(new_message).execute()
