a3a6da9df920ac803266a584cc1a8ed8e57d1457
yhecb <148933237+yhecb@users.noreply.github.com>
2025-03-17 11:50:14 +0800

    Add setup_realtime

---
+++ database/setup_realtime.sql
@@ -0,0 +1,14 @@
+begin;
+
+-- remove the supabase_realtime publication
+drop
+  publication if exists supabase_realtime;
+
+-- re-create the supabase_realtime publication with no tables
+create publication supabase_realtime;
+
+commit;
+
+-- add the table 'journey_messages' to the publication
+alter
+  publication supabase_realtime add table journey_messages;
