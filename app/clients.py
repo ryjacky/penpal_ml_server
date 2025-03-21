4b7aa6c6dff8088d0a23e58c6db42d8284e615a8
ryjacky127 <ryjacky127@gmail.com>
2025-03-21 15:52:02 +0900

    adds global client variables

---
+++ app/clients.py
@@ -0,0 +1,10 @@
+import os
+
+from supabase import create_client, Client
+
+from modules.llm.llm_client import LLMClient
+from modules.llm.prototype_llm_client import PrototypeLLMClient
+
+supabase_client: Client = create_client(os.environ.get("SUPABASE_URL"), os.environ.get("SUPABASE_KEY"))
+
+chat_client: LLMClient = PrototypeLLMClient()
